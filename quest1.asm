3d597084830b5a7b46074e0d49a71e00c7a921ed
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2022-07-06 17:04:05 -0300

    Update quest1.asm

--- quest1.asm
+++ quest1.asm
@@ -1,64 +1,66 @@
 # Questão 1: Escreva um algoritmo que calcule a divisão inteira e o resto da divivsão de dois números.
 
-.data 
-	dividendo: .asciiz "Informe um número inteiro positivo >DIVIDENDO: "
-	divisor: .asciiz "\nInforme um número inteiro positivo >DIVISOR: "
-	quociente: .asciiz "\nQuociente: "
-	resto_divisao: .asciiz "\nResto: "
-.text 
+.data
+promptDiviendo: .asciiz "Insira um numero inteiro positivo correspondente ao dividendo: "
+promptDivisor: .asciiz "Insira um numero inteiro positivo correspondente ao divisor: "
+outputQuociente: .asciiz "\nQuociente = "
+outputResto: .asciiz "\nResto = "
 
+.text 
 main:
-	
-	#Dividendo
-	li $v0,4 
-	la $a0,dividendo 
-	syscall
-	
-	li $v0,5 
-	syscall
-	
-	move $s0,$v0 
-	
-	#Divisor
-	li $v0,4 
-	la $a0,divisor 
-	syscall
-	
-	li $v0,5 
-	syscall
-	
-	move $s1,$v0 
-	
-	move $s2,$s0 #resto = dividendo
-	li $s3,0 #quociente = 0
-	
-	#Realiza o calculo
-	 Loop:
-	 slt $t0,$s2,$s1      #t0 = s2 < s1 (resto < divisor)
-	 bne $t0,$zero,Exit   #se t0 != 0, Exit
-	 sub $s2,$s2,$s1      #s2 = s2 - s1 (resto = resto - divisor)
-	 addi $s3,$s3,1       #s3 = s3 + 1 (quociente + 1)
-	 j Loop             
-	 Exit: 
-		
-	#print quociente
-	li $v0,4
-	la $a0,quociente
-	syscall
-	
-	li $v0,1
-	move $a0,$s3
-	syscall
-	
-	#print resto
-	li $v0,4
-	la $a0,resto_divisao
-	syscall
-	
-	li $v0,1
-	move $a0,$s2
-	syscall
-	
-	#Termina o programa
-  	li $v0, 10
-	syscall 
+    # Exibir mensagem dividendo
+    la $a0, promptDiviendo
+    li $v0, 4
+    syscall
+
+    # Ler o inteiro inserido (dividendo) 
+    li $v0, 5
+    syscall
+    
+    # Salva o dividendo em $s0
+    move $s0, $v0
+
+    # Exibir mensagem divisor
+    la $a0, promptDivisor
+    li $v0, 4
+    syscall
+
+    # Ler o inteiro inserido (divisor)
+    li $v0, 5
+    syscall
+    
+    # Salva o divisor em $s1
+    move $s1, $v0
+
+    move $t0, $s0      # resto = diviendo
+    addi $t1, $zero, 0 # quociente = 0
+
+    loop: blt  $t0, $s1, end_loop    # se resto < divisor, Exit
+          sub  $t0, $t0, $s1         # resto = resto - divisor 
+          addi $t1, $t1, 1           # quociente ++
+          j loop                     # retorna para o loop
+    end_loop:                     
+
+    # Exibir mensagem quociente
+    li $v0, 4
+    la $a0, outputQuociente
+    syscall
+
+    # Imprimir Quociente
+    li $v0, 1
+    move $a0, $t1
+    syscall
+
+    # Exibir mensagem resto
+    li $v0, 4
+    la $a0, outputResto
+    syscall
+
+    # Imprimir resto
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    # Terminar programa
+    li $v0, 10
+    syscall
